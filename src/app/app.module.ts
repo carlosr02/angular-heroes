857075b8185eff95f77c69a9edc75aa1f3363484
carlosr02 <carlosr-02@live.com>
2017-07-15 18:36:19 -0300

    Modificações do 5o capítulo em https://angular.io/tutorial/toh-pt5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './details/hero-detail.component';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { HeroDetailComponent } from './details/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroService } from './hero.service';
+
+import { AppRoutingModule } from  './app-routing.module'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
